8d0e7585c42f01aa9cbaf053c929add72214a50b
Ahmed Khemiri <ahmed@upperskills.com>
2017-10-13 09:55:47 +0100

    Initial Commit

---
+++ HandyMan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HandyMan
+//
+//  Created by Ahmed Khemiri on 10/13/17.
+//  Copyright © 2017 Ahmed Khemiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
