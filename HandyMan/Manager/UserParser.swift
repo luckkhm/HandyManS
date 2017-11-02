23ff9cc4fac12f18e98ca5145b89030cbb38d958
Ahmed Khemiri <ahmedkhemirigl@gmail.com>
2017-11-02 12:45:20 +0100

    [first commit]

---
+++ HandyMan/Manager/UserParser.swift
@@ -0,0 +1,13 @@
+//
+//  UserParser.swift
+//  HandyMan
+//
+//  Created by Ahmed Khemiri on 10/13/17.
+//  Copyright © 2017 Ahmed Khemiri. All rights reserved.
+//
+
+import UIKit
+
+class UserParser: NSObject {
+
+}
